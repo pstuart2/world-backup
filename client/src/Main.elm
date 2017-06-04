761026935a5678d40c21eef70379a9ecd70b69c7
pstuart2 <pstuart2@gmail.com>
2017-06-04 13:18:51 -0500

    Part 3a of the tutorial

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,16 +1,21 @@
 module Main exposing (..)
 
 import Commands exposing (fetchPlayers)
-import Html exposing (program)
 import Models exposing (Model, initialModel)
 import Msgs exposing (Msg)
+import Navigation exposing (Location)
+import Routing
 import Update exposing (update)
 import View exposing (view)
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, fetchPlayers )
+init : Location -> ( Model, Cmd Msg )
+init location =
+    let
+        currentRoute =
+            Routing.parseLocation location
+    in
+    ( initialModel currentRoute, fetchPlayers )
 
 
 subscriptions : Model -> Sub Msg
@@ -24,7 +29,7 @@ subscriptions model =
 
 main : Program Never Model Msg
 main =
-    program
+    Navigation.program Msgs.OnLocationChange
         { init = init
         , view = view
         , update = update
