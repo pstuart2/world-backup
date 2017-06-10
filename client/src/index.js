a0855ff3a6e1f0a46533d685678b6ad7c154dc8f
pstuart2 <pstuart2@gmail.com>
2017-06-09 21:54:19 -0500

    Better webpack

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,6 @@
 require('ace-css/css/ace.css');
 require('font-awesome/css/font-awesome.css');
 
-// Require index.html so it gets copied to dist
-require('./index.html');
-
 var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,53 +1,88 @@
 var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    app: [
-      './src/index.js'
-    ]
-  },
+function getEntry() {
+  const result = [];
 
-  output: {
-    path: path.resolve(__dirname + '/dist'),
-    filename: '[name].js',
-  },
+  // the entry point of our app
+  result.push('./src/index.js');
 
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
+  return result;
+}
+
+function getSettings() {
+  let settings = {};
+  const isProd = process.env.NODE_ENV === 'production';
+
+  settings.baseUrl = isProd ? '' : 'http://localhost:3030';
+
+  return settings;
+}
+
+function getPlugins() {
+  return [
+    new HtmlWebpackPlugin(({
+      template: './src/index.html',
+      settings: JSON.stringify(getSettings())
+    }))
+  ];
+}
+
+const config = function(env) {
+  return {
+    entry: getEntry(),
+
+    output: {
+      path: path.resolve(__dirname + '/dist'),
+      filename: 'app.js',
+      publicPath: '/'
+    },
+
+    plugins: getPlugins(),
+
+    module: {
+      rules: [{
+          test: /\.(css|scss)$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ]
+        },
+        {
+          test: /\.elm$/,
+          exclude: [/elm-stuff/, /node_modules/],
+          loader: 'elm-webpack-loader?verbose=true&warn=true',
+        },
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader',
+        },
+      ],
+
+      noParse: /\.elm$/,
+    },
+
+    devServer: {
+      inline: true,
+      stats: {
+        colors: true
       },
-      {
-        test:    /\.html$/,
-        exclude: /node_modules/,
-        loader:  'file-loader?name=[name].[ext]',
-      },
-      {
-        test:    /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true',
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
-      },
-    ],
-
-    noParse: /\.elm$/,
-  },
-
-  devServer: {
-    inline: true,
-    stats: { colors: true },
-  },
+    },
 
 
+  };
+}
+
+module.exports = function(env) {
+  if (!env)
+    env = {};
+
+  console.log('Node Env: ' + process.env.NODE_ENV)
+  console.log(env);
+  
+  return config(env);
 };
